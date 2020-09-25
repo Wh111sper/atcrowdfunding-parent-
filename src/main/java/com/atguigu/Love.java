18d24efcf71ed83b6c7aea033299fdeef20f21f1
毛语剑 <2597828470@qq.com>
2020-09-25 16:27:53 +0800

    IDEA中使用git

---
+++ src/main/java/com/atguigu/Love.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author MaoYuJian
+ * @create 2020-09-25
+ */
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("今天是个好日子");
+    }
+}
