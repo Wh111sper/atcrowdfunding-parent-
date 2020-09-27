3c77b20d2ccb00c2e9723f3f82f94a28a2110c7c
郑帅哥 <1608200596@qq.com>
2020-09-27 19:17:13 +0800

    郑帅康

--- src/main/java/com/atguigu/Love.java
+++ src/main/java/com/atguigu/Love.java
@@ -8,5 +8,6 @@ public class Love {
     public static void main(String[] args) {
         System.out.println("今天是个好日子");
         System.out.println("6666666");
+        System.out.println("zsk到此一游");
     }
 }
